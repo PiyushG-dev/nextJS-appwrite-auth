bbcd0da378c390307641a12cb9e12faff650193e
PiyushG-dev <pjgpiyush@gmail.com>
2024-01-20 20:15:49 +0530

    routing added

---
+++ src/app/register/page.jsx
@@ -0,0 +1,9 @@
+"use client";
+import React from "react";
+import styles from "../styles/Register.module.css";
+
+const Register = () => {
+  return <div>Register</div>;
+};
+
+export default Register;
