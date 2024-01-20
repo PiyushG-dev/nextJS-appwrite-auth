e049dc24bf75670efd1e67405b4d8d79028e7e65
PiyushG-dev <pjgpiyush@gmail.com>
2024-01-20 20:31:01 +0530

    project re-start

--- src/app/login/page.jsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import React from "react";
-import styles from "../styles/Login.module.css";
-
-const Login = () => {
-  return <div>Login</div>;
-};
-
-export default Login;
--- src/app/register/page.jsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import React from "react";
-import styles from "../styles/Register.module.css";
-
-const Register = () => {
-  return <div>Register</div>;
-};
-
-export default Register;
