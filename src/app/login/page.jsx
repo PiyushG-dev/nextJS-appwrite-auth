1d88ba05322bc59e02f59b5096ce681091bcbc44
PiyushG-dev <pjgpiyush@gmail.com>
2024-01-21 19:29:26 +0530

    login page ui added

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -7,24 +7,35 @@ const Login = () => {
   const [password, setPassword] = useState("");
 
   return (
-    <div className={styles.container}>
-      <form>
-        <label>Login form</label>
-        <input
-          type="email"
-          placeholder="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button>login</button>
-      </form>
-    </div>
+    <main className={styles.wrapper}>
+      <div className={styles.left}>
+        <p>log in</p>
+        <h1>Welcome Back</h1>
+        <p>
+          Sign back in into the most sophisticated authentication system in this
+          world built in next
+        </p>
+      </div>
+      <div className={styles.right}>
+        <h2>your account</h2>
+        <form>
+          <h3>email</h3>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <h3>password</h3>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button>login</button>
+        </form>
+        <hr />
+      </div>
+    </main>
   );
 };
 
